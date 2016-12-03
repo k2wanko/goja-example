3519cc6f2ca73743e99ad7425cb70dd71fb1bc14
Kazuhiro Kubota <k2.wanko@gmail.com>
2016-12-03 18:14:48 +0900

    Add module

--- main.go
+++ main.go
@@ -4,13 +4,26 @@ import (
 	"fmt"
 
 	"github.com/dop251/goja"
+	"github.com/dop251/goja_nodejs/require"
 )
 
 func main() {
+	registry := new(require.Registry)
+
 	vm := goja.New()
-	v, err := vm.RunString(`2+2`)
-	if err != nil {
+	rm := registry.Enable(vm)
+
+	var v goja.Value
+	if sum, err := rm.Require("sum.js"); err != nil {
 		panic(err)
+	} else {
+		if sum, ok := goja.AssertFunction(sum); ok {
+			v, err = sum(goja.Undefined(), vm.ToValue(6), vm.ToValue(7))
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
+
 	fmt.Printf("%T, %#v\n", v, v)
 }
